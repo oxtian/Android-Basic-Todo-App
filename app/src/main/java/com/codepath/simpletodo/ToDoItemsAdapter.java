db83f667cd6ef5478c798fd9e2e1b553808a57de
Xin Tian Oon <xintian89@gmail.com>
2017-01-22 18:54:25 +0800

    Improve style of the todo items in the list using a custom adapter.

--- app/src/main/java/com/codepath/simpletodo/ToDoItemsAdapter.java
+++ app/src/main/java/com/codepath/simpletodo/ToDoItemsAdapter.java
@@ -11,9 +11,15 @@ import com.codepath.simpletodo.db.ToDoItem;
 
 import java.util.ArrayList;
 
+import static com.codepath.simpletodo.R.id.tvItem;
+
 public class ToDoItemsAdapter extends ArrayAdapter<ToDoItem> {
     public ToDoItemsAdapter(Context context, ArrayList<ToDoItem> todoItems) {
         super(context, 0, todoItems);
+    }
+
+    static class ViewHolder {
+        TextView itemView;
     }
 
     @Override
@@ -21,13 +27,17 @@ public class ToDoItemsAdapter extends ArrayAdapter<ToDoItem> {
         // Get the data item for this position
         ToDoItem todoItem = getItem(position);
         // Check if an existing view is being reused, otherwise inflate the view
+        ViewHolder holder;
         if (convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.to_do_item, parent, false);
+            holder = new ViewHolder();
+            holder.itemView = (TextView) convertView.findViewById(tvItem);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
         }
 
-        TextView tvItem = (TextView) convertView.findViewById(R.id.tvItem);
-        tvItem.setText(todoItem.item);
-
+        holder.itemView.setText(todoItem.item);
         // Return the completed view to render on screen
         return convertView;
     }
